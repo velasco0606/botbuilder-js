1953a07d35d885b58873168ce49ec33af5fe4914
Steven Ickman <stevenic@microsoft.com>
2019-06-05 19:40:37 -0700

    Updated EndDialog step to support expressions.

--- libraries/botbuilder-dialogs-adaptive/src/steps/endDialog.ts
+++ libraries/botbuilder-dialogs-adaptive/src/steps/endDialog.ts
@@ -5,43 +5,63 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import { DialogTurnResult, DialogConfiguration, DialogCommand, DialogContext } from 'botbuilder-dialogs';
+import { DialogTurnResult, DialogConfiguration, DialogContext, Dialog } from 'botbuilder-dialogs';
+import { ExpressionPropertyValue, ExpressionProperty } from '../expressionProperty';
 
 export interface EndDialogConfiguration extends DialogConfiguration {
     /**
      * (Optional) specifies an in-memory state property that should be returned to the calling
      * dialog.
      */
-    resultProperty?: string;
+    returnValue?: ExpressionPropertyValue<any>;
 }
 
-export class EndDialog extends DialogCommand {
+export class EndDialog extends Dialog {
 
     /**
      * Creates a new `EndDialog` instance.
-     * @param resultProperty (Optional) in-memory state property to return to the called dialog.
+     * @param returnValue (Optional) in-memory state property to return to the called dialog.
      */
-    constructor(resultProperty?: string) {
+    constructor(returnValue?: ExpressionPropertyValue<any>) {
         super();
-        if (resultProperty) { this.resultProperty = resultProperty }
+        this.inheritState = true;
+        if (returnValue) { this.returnValue = new ExpressionProperty(returnValue) }
     }
 
     public configure(config: EndDialogConfiguration): this {
-        return super.configure(config);
+        for (const key in config) {
+            if (config.hasOwnProperty(key)) {
+                const value = config[key];
+                switch(key) {
+                    case 'returnValue':
+                        this.returnValue = new ExpressionProperty(value);
+                        break;
+                    default:
+                        super.configure({ [key]: value });
+                        break;
+                }
+            }
+        }
+
+        return this;
     }
 
     protected onComputeID(): string {
-        return `end[${this.hashedLabel(this.resultProperty || '')}]`;
+        const label = this.returnValue ? this.returnValue.toString() : '';
+        return `endDialog[${this.hashedLabel(label)}]`;
     }
 
     /**
      * (Optional) specifies an in-memory state property that should be returned to the calling
      * dialog.
      */
-    public resultProperty: string;
+    public returnValue?: ExpressionProperty<any>;
 
-    protected async onRunCommand(dc: DialogContext): Promise<DialogTurnResult> {
-        const result = this.resultProperty ? dc.state.getValue(this.resultProperty) : undefined;
-        return await this.endParentDialog(dc, result);
+    public async beginDialog(dc: DialogContext): Promise<DialogTurnResult> {
+        if (!dc.parent) { throw new Error(`${this.id}: step should only ever be used within a container dialog.`) }
+
+        // Compute return value and end parent dialog
+        const returnValue = this.returnValue ? this.returnValue.evaluate(this.id, dc.state.toJSON()) : undefined;
+        return await dc.parent.endDialog(returnValue);
     }
 }
