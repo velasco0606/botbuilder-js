23da0133337dd8a3a857ac3ed064d412ba5a36e1
Edwin Bwambale <edwinb@microsoft.com>
2019-10-08 09:21:49 -0700

    PR Cleanup

--- libraries/botframework-connector/src/tokenApi/tokenApiClientContext.ts
+++ libraries/botframework-connector/src/tokenApi/tokenApiClientContext.ts
@@ -32,7 +32,6 @@ export class TokenApiClientContext extends msRest.ServiceClient {
     super(credentials, options);
 
     this.baseUri = options.baseUri || this.baseUri || "https://token.botframework.com";
-    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@: " + this.baseUri);
     this.requestContentType = "application/json; charset=utf-8";
     this.credentials = credentials;
 
