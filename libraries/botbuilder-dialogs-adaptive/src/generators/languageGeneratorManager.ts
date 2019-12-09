12360db07df446225f6aeeb538ed7e7ba9e2a1ea
Shuai Wang <shuwan@microsoft.com>
2019-12-09 19:29:50 +0800

    add some change

--- libraries/botbuilder-dialogs-adaptive/src/generators/languageGeneratorManager.ts
+++ libraries/botbuilder-dialogs-adaptive/src/generators/languageGeneratorManager.ts
@@ -10,6 +10,66 @@
  * Class which manages cache of all LG resources from a ResourceExplorer. 
  * This class automatically updates the cache when resource change events occure.
  */
+import { IResource, ResourceExplorer, FileResource } from '../../../botbuilder-dialogs-declarative/src';
+import { MultiLanguageResourceLoader } from '../multiLanguageResourceLoader';
+import { LanguageGenerator } from '../languageGenerator'
+import { TemplateEngineLanguageGenerator } from './templateEngineLanguageGenerator';
+import { normalize, extname } from 'path';
+
 export class LanguageGeneratorManager {
-    // TODO
+    private _resourceExporer: ResourceExplorer;
+    
+    /// <summary>
+    /// multi language lg resources. en -> [resourcelist].
+    /// </summary>
+    private readonly _multilanguageResources: Map<string, IResource[]>;
+
+    public constructor(resourceManager: ResourceExplorer) {
+        this._resourceExporer = resourceManager;
+        this._multilanguageResources = MultiLanguageResourceLoader.load(resourceManager);
+
+        // load all LG resources
+        this._resourceExporer.getResources("lg").then(
+            resourses => {resourses.forEach(
+                resourse => {
+                    this._languageGenerator[resourse.id()] = this.getTemplateEngineLanguageGenerator(resourse);
+                }
+            );
+        //this._resourceExporer.Changed += ResourceExplorer_Changed;
+        }); 
+    }
+    
+    public _languageGenerator: Map<string, LanguageGenerator> = new Map<string, LanguageGenerator>();
+
+    public static resourceExplorerResolver(locale: string, id: string, resourceMapping: Map<string, IResource[]>): Function {
+        return (source: string, id: string) => {
+            const fallbaclLocale = MultiLanguageResourceLoader.FallbackLocale(locale, Array.from(resourceMapping.keys()));
+            const resources: IResource[] = resourceMapping[fallbaclLocale];
+
+            const resourceName = normalize(id);
+            const resource:IResource = resources.filter(u => {
+                MultiLanguageResourceLoader.ParseLGFileName(u.id).prefix.toLowerCase() === MultiLanguageResourceLoader.ParseLGFileName(resourceName).prefix.toLowerCase();
+            })[0];
+
+            if (resource === undefined) {
+                return {content:"", path: resource.id()};
+            } else {
+                resource.readText().then(
+                    text => {
+                        return {content: text, path: resource.id()};
+                    }
+                );
+            }
+        }
+    }
+
+    private  ResourceExplorer_Changed(resources: IResource[]): void {
+        resources.filter(u => extname(u.id()).toLowerCase() === '.lg').forEach(resource => 
+            this._languageGenerator[resource.id()] = this.getTemplateEngineLanguageGenerator(resource))
+    }
+
+    private getTemplateEngineLanguageGenerator(resource: IResource): TemplateEngineLanguageGenerator {
+        FileResource
+        return new TemplateEngineLanguageGenerator(await resource.readText(), resource.id(), this._multilanguageResources);
+    }
 }
--- libraries/botbuilder-dialogs-adaptive/src/multiLanguageResourceLoader.ts
+++ libraries/botbuilder-dialogs-adaptive/src/multiLanguageResourceLoader.ts
@@ -1,3 +1,6 @@
+import { IResource } from "../../botbuilder-dialogs-declarative/src";
+import { ResourceExplorer } from '../../botbuilder-dialogs-declarative/src'
+import { LanguagePolicy } from  './languagePolicy'
 /**
  * @module botbuilder-dialogs-adaptive
  */
@@ -10,10 +13,113 @@
  * load all lg resource and split them into different language group.
  */
 export class MultiLanguageResourceLoader {
-    // TODO
-    /*
-    public static load(resourceExplorer: ResourceExplorer) {
+    public static load(resourceExplorer: ResourceExplorer): Map<string, IResource[]> {
+        const resourceMapping: Map<string, IResource[]> = new Map<string, IResource[]>();
+        const allResouces: IResource[] = await resourceExplorer.getResources("lg");
+        const languagePolicy = LanguagePolicy.getDefaultPolicy();
+        for (const locale in languagePolicy) {
+            let suffixs = languagePolicy[locale];
+            const existNames = new Set<string>();
+            for (const index in suffixs) {
+                const suffix = suffixs[index];
+                if ((locale === undefined || locale ==="") || (suffix !== undefined && suffix !== "")) {
+                    const resourcesWithSuffix = allResouces.filter(u => ParseLGFileName(u.id() === suffix));
+                    resourcesWithSuffix.forEach(u => {
+                        const resourceName = u.id();
+                        const length = (suffix !== undefined && suffix !== "")? 3 : 4;
+                        const prefixName = resourceName.substring(0, resourceName.length - suffix.length - length);
+                        if (!existNames.has(prefixName)) {
+                            existNames.add(prefixName);
+                            if (!resourceMapping.has(locale)) {
+                                resourceMapping.set(locale, [u]);
+                            } else {
+                                resourceMapping[locale].add(u);
+                            }
+                        }
+                    });
+                } else {
+                    if (resourceMapping.has(locale)) {
+                        const resourcesWithEmptySuffix = allResouces.filter(u => this.ParseLGFileName(u.id()).language === "");
+                        resourcesWithEmptySuffix.forEach(u => {
+                            const resourceName = u.id();
+                            const prefixName = resourceName.substring(0, resourceName.length - 3);
+                            if (!existNames.has(prefixName)) {
+                                existNames.add(prefixName);
+                                resourceMapping[locale].add(u);
+                            }
+                        });
+                    }
+                }
+            }
+        }
 
+        return fallbackMultiLangResource(resourceMapping);
     }
-    */
+
+    public static ParseLGFileName(lgFileName: string): lgFileParsedResult {
+        if (lgFileName === undefined || !lgFileName.endsWith(".lg")) {
+            return {prefix: lgFileName, language: ""};
+        }
+
+        const fileName = lgFileName.substring(0, lgFileName.length - ".lg".length);
+        const lastDot = fileName.lastIndexOf(".");
+        if (lastDot > 0) {
+            return {prefix: fileName.substring(0, lastDot), language: fileName.substring(lastDot + 1)}
+        } else {
+            return {prefix: fileName, language: ""};
+        }
+    }
+
+    public static fallbackLocale(locale: string, optionalLocales: string[]) {
+        if (optionalLocales === undefined) {
+            throw new TypeError("Invalid Arguments");
+        }
+
+        if (optionalLocales.includes(locale)) {
+            return locale;
+        }
+
+        const languagePolicy = LanguagePolicy.getDefaultPolicy();
+        if (languagePolicy.has(locale)) {
+            const fallbackLocales = languagePolicy[locale];
+            for (const i in fallbackLocales) {
+                const fallbackLocale = fallbackLocales[i]
+                if (optionalLocales.includes(fallbackLocale)) {
+                    return fallbackLocale;
+                }
+            }
+        } else if (optionalLocales.includes("")) {
+            return "";
+        }
+
+        throw Error(`there is no locale fallback for ${locale}`);
+    }
+
+    private static fallbackMultiLangResource(resourceMapping: Map<string, IResource[]>): Map<string, IResource[]> {
+        const resourcePoolDict = new Map<string, IResource[]>();
+        for (const currentLocale of resourceMapping.keys()) {
+            const currentResourcePool = resourceMapping[currentLocale];
+            const existLocale  = this.matchingPoolDict(currentResourcePool, resourceMapping);
+            
+            if (existLocale === undefined) {
+                resourcePoolDict.set(currentLocale, currentResourcePool);
+            } else {
+                const newLocale: string = this.findCommonAncestorLocale(existLocale, currentLocale);
+                if (!(newLocale === undefined || newLocale.trim() === "")) {
+                    resourcePoolDict.delete(existLocale);
+                    resourcePoolDict.set(newLocale, currentResourcePool);
+                }
+            }
+        }
+
+        return resourcePoolDict;
+    }
+
+    
+
 }
+
+type lgFileParsedResult = {
+    prefix: string, 
+    language: string
+} 
