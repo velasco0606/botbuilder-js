1223cdb55d308c2aa4d8a08686bf63e8b2100126
hond <hond@microsoft.com>
2019-12-25 15:54:49 +0800

    fix eslint

--- libraries/botbuilder-dialogs-declarative/tests/fileResource.test.js
+++ libraries/botbuilder-dialogs-declarative/tests/fileResource.test.js
@@ -5,21 +5,21 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
-describe('FileResource', function () {
+describe('FileResource', function() {
     this.timeout(5000);
 
-    it('FileResource load existing file relative path', async () => {
-        const fileResource = new FileResource(`${__dirname}/resources/00 - TextPrompt/SimplePrompt.main.dialog`);
+    it('FileResource load existing file relative path', () => {
+        const fileResource = new FileResource(`${ __dirname }/resources/00 - TextPrompt/SimplePrompt.main.dialog`);
         assert.equal(fileResource.id(), 'SimplePrompt.main.dialog');
-        const text = await fileResource.readText();
+        const text = fileResource.readText();
         assert.equal(text[0], '{');
     });
 
-    it('FileResource load existing file absolute path', async () => {
-        const absolutePath = path.resolve(`${__dirname}/resources/00 - TextPrompt/SimplePrompt.main.dialog`);
+    it('FileResource load existing file absolute path', () => {
+        const absolutePath = path.resolve(`${ __dirname }/resources/00 - TextPrompt/SimplePrompt.main.dialog`);
         const fileResource = new FileResource(absolutePath);
         assert.equal(fileResource.id(), 'SimplePrompt.main.dialog');
-        const text = await fileResource.readText();
+        const text = fileResource.readText();
         assert.equal(text[0], '{');
     });
 });
--- libraries/botbuilder-dialogs-declarative/tests/typeLoader.test.js
+++ libraries/botbuilder-dialogs-declarative/tests/typeLoader.test.js
@@ -6,112 +6,112 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
-describe('TypeLoader', function () {
+describe('TypeLoader', function() {
     this.timeout(5000);
 
-    it('TypeLoader TextPrompt: simple textprompt dialog should prompt for text', async function () {
+    it('TypeLoader TextPrompt: simple textprompt dialog should prompt for text', async function() {
         const adapter = await declarativeTestCase('00 - TextPrompt/SimplePrompt.main.dialog', null);
 
         await adapter.send('hi')
-                .assertReply('Hello, I\'m Zoidberg. What is your name?');
+            .assertReply('Hello, I\'m Zoidberg. What is your name?');
     });
 
-    it('TypeLoader Steps: should be ran when no other rules', async function () {
+    it('TypeLoader Steps: should be ran when no other rules', async function() {
         const adapter = await declarativeTestCase('01 - Steps/Steps.main.dialog', null);
 
         await adapter.send('hi')
-                .assertReply('Action 1')
-                .assertReply('Action 2')
-                .assertReply('Action 3');
+            .assertReply('Action 1')
+            .assertReply('Action 2')
+            .assertReply('Action 3');
     });
 
-    it('TypeLoader EndTurn: end turn should wait for user input', async function () {
+    it('TypeLoader EndTurn: end turn should wait for user input', async function() {
         const adapter = await declarativeTestCase('02 - EndTurn/EndTurn.main.dialog', null);
 
         await adapter
             .send('hi')
-                .assertReply('What\'s up?')
+            .assertReply('What\'s up?')
             .send('not much homie')
-                .assertReply('Oh I see!');
+            .assertReply('Oh I see!');
     });
 
-    it('TypeLoader IfCondition: if name is null then ask, if not will say hi', async function () {
+    it('TypeLoader IfCondition: if name is null then ask, if not will say hi', async function() {
         const adapter = await declarativeTestCase('03 - IfCondition/IfCondition.main.dialog', null);
         await adapter
             .send('hi')
-                .assertReply("Hello, I'm Zoidberg. What is your name?")
+            .assertReply('Hello, I\'m Zoidberg. What is your name?')
             .send('Wendy')
-                .assertReply('Hello Wendy, nice to talk to you!')
+            .assertReply('Hello Wendy, nice to talk to you!');
     });
 
-    it('TypeLoader TextInput: should prompt for name and then use name in response. Next time prompt should not ask for property that is already in memory', async function () {
+    it('TypeLoader TextInput: should prompt for name and then use name in response. Next time prompt should not ask for property that is already in memory', async function() {
         const adapter = await declarativeTestCase('04 - TextInput\\TextInput.main.dialog', null);
 
         await adapter
             .send('hi')
-                .assertReply('Hello, I\'m Zoidberg. What is your name?')
+            .assertReply('Hello, I\'m Zoidberg. What is your name?')
             .send('Carlos')
-                .assertReply('Hello Carlos, nice to talk to you!')
+            .assertReply('Hello Carlos, nice to talk to you!');
     });
 
-    it('TypeLoader DoSteps: if user says joke, then should go into the joke flow; if user says fortune, then should go into the fortune flow', async function () {
+    it('TypeLoader DoSteps: if user says joke, then should go into the joke flow; if user says fortune, then should go into the fortune flow', async function() {
         const adapter = await declarativeTestCase('06 - DoSteps/DoSteps.main.dialog', '06 - DoSteps');
         await adapter
             .send('hi')
-                .assertReply('Hello, I\'m Zoidberg. What is your name?')
+            .assertReply('Hello, I\'m Zoidberg. What is your name?')
             .send('Rose')
-                .assertReply('Hello Rose, nice to talk to you!')
+            .assertReply('Hello Rose, nice to talk to you!')
             .send('joke')
-                .assertReply('Why did the chicken cross the road?')
+            .assertReply('Why did the chicken cross the road?')
             .send('why')
-                .assertReply('To get to the other side')
+            .assertReply('To get to the other side')
             .send('fortune')
-                .assertReply('Seeing into the future...')
-                .assertReply('I see great things happening...')
-                .assertReply('Perhaps even a successful bot demo')
+            .assertReply('Seeing into the future...')
+            .assertReply('I see great things happening...')
+            .assertReply('Perhaps even a successful bot demo');
     });
 
-    it('TypeLoader BeginDialog: should extract two intent flows into two dialog files', async function () {
+    it('TypeLoader BeginDialog: should extract two intent flows into two dialog files', async function() {
         const adapter = await declarativeTestCase('07 - BeginDialog/BeginDialog.main.dialog', '07 - BeginDialog');
         await adapter
             .send('hi')
-                .assertReply('Hello, I\'m Zoidberg. What is your name?')
+            .assertReply('Hello, I\'m Zoidberg. What is your name?')
             .send('Bender')
-                .assertReply('Hello Bender, nice to talk to you!')
+            .assertReply('Hello Bender, nice to talk to you!')
             .send('tell me a joke')
-                .assertReply('Why did the chicken cross the road?')
+            .assertReply('Why did the chicken cross the road?')
             .send('why?')
-                .assertReply('To get to the other side')
+            .assertReply('To get to the other side')
             .send('tell my fortune')
-                .assertReply('Seeing into the future...')
-                .assertReply('I see great things in your future...')
-                .assertReply('Potentially a successful demo')
+            .assertReply('Seeing into the future...')
+            .assertReply('I see great things in your future...')
+            .assertReply('Potentially a successful demo');
     });
 
-    it('TypeLoader EndDialog: should cancel dialog while saying cancel or never mind', async function () {
+    it('TypeLoader EndDialog: should cancel dialog while saying cancel or never mind', async function() {
         const adapter = await declarativeTestCase('09 - EndDialog/EndDialog.main.dialog', '09 - EndDialog');
         await adapter
             .send('hi')
-                .assertReply('Hello, I\'m Zoidberg. What is your name?')
+            .assertReply('Hello, I\'m Zoidberg. What is your name?')
             .send('Alex')
-                .assertReply('Hello Alex, nice to talk to you!')
-                .assertReply("I'm a joke bot. To get started say \"joke\".")
+            .assertReply('Hello Alex, nice to talk to you!')
+            .assertReply('I\'m a joke bot. To get started say "joke".')
             .send('tell me a joke')
-                .assertReply('Why did the chicken cross the road?')
+            .assertReply('Why did the chicken cross the road?')
             .send('why?')
-                .assertReply('To get to the other side')
+            .assertReply('To get to the other side')
             .send('cancel')
-                .assertReply('ok.')
+            .assertReply('ok.');
     });
 });
 
 async function declarativeTestCase(path, resourcesFolder) {
-    const json = await readPackageJson(`libraries/botbuilder-dialogs-declarative/tests/resources/${path}`);
+    const json = await readPackageJson(`tests/resources/${ path }`);
     let loader = new TypeLoader();
 
     if (resourcesFolder) {
         let resourceExplorer = new ResourceExplorer();
-        resourceExplorer.addFolder(`./libraries/botbuilder-dialogs-declarative/tests/resources`);
+        resourceExplorer.addFolder(`./tests/resources`);
         loader = new TypeLoader(null, resourceExplorer);
     }
 
@@ -134,10 +134,10 @@ async function declarativeTestCase(path, resourcesFolder) {
 
 function readPackageJson(path) {
     return new Promise((resolve, reject) => {
-        fs.readFile(path, function (err, buffer) {
+        fs.readFile(path, function(err, buffer) {
             if (err) { reject(err); }
             var json = JSON.parse(buffer.toString().trim());
             resolve(json);
-          });
+        });
     });
 }
